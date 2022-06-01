0d8c31dfa7d8d2ba3839d2a45840e84bdef03c05
Levani <levani.papashvili@studiolabs.com>
2022-06-01 14:03:53 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 
 
@@ -27,7 +29,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
